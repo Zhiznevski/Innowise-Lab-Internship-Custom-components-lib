cd866dcf8855f07034a7377ca4294e438768767d
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-03 01:09:27 +0300

    feat: add basic checkbox component

---
+++ src/Checkbox/Checkbox.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './Checkbox.module.css';
+
+export interface CheckboxProps {
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: () => void;
+  label?: React.ReactNode;
+}
+
+function Checkbox({
+  checked = false,
+  disabled = false,
+  onChange = () => {},
+  label = 'checkbox',
+}: CheckboxProps) {
+  return (
+    <div className={styles.wrapper}>
+      <input
+        className={styles.input}
+        id="cbx"
+        type="checkbox"
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+      />
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label className={styles.checkbox} htmlFor="cbx" />
+      <label className={styles.label} htmlFor="cbx">
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default Checkbox;
