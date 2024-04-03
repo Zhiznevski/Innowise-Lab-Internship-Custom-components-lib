bafdaff0b676e8da06e0ce495414e8e290626b45
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-03 15:53:07 +0300

    feat: add Modal component

---
+++ src/Modal/Modal.tsx
@@ -0,0 +1,45 @@
+import { createPortal } from 'react-dom';
+import React, { useState } from 'react';
+import styles from './Modal.module.css';
+import Button from '../Button/Button';
+
+export interface ModalProps {
+  open: boolean;
+  onClose?: () => void;
+  children?: React.ReactElement;
+}
+export function MockChildren() {
+  return (
+    <>
+      <h2 className={styles.title}>Title in a modal</h2>
+      <p className={styles.description}>Description in a modal</p>
+    </>
+  );
+}
+
+export function Modal({ open = false, onClose = () => {}, children }: ModalProps) {
+  if (!open) return null;
+  return (
+    <div className={styles.windowWrapper}>
+      <div aria-hidden onClick={onClose} className={styles.background} />
+      <div className={styles.window}>{children}</div>
+    </div>
+  );
+}
+
+export default function ModalApp() {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+  return (
+    <>
+      <Button onClick={handleOpen} />
+      {createPortal(
+        <Modal open={open} onClose={handleClose}>
+          <MockChildren />
+        </Modal>,
+        document.body
+      )}
+    </>
+  );
+}
