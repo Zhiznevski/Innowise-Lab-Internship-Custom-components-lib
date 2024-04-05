484e47336f468e4c93e4b73a85f3e1166cfe7581
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-05 12:14:47 +0300

    feat: export components from npm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Button from './Button/Button';
+import Checkbox from './Checkbox/Checkbox';
+import ModalApp from './Modal/Modal';
 import Switch from './Switch/Switch';
+import TextField from './TextField/TextField';
 
-export { Button, Switch };
+export { Button, Switch, Checkbox, ModalApp, TextField };
