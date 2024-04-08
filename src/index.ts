593a45e4dbd89ef95b3bc0130a665330b9f83f00
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-08 12:45:31 +0300

    feat: export select component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Button from './Button/Button';
 import Checkbox from './Checkbox/Checkbox';
 import ModalApp from './Modal/Modal';
+import Select from './Select/Select';
 import Switch from './Switch/Switch';
 import TextField from './TextField/TextField';
 
-export { Button, Switch, Checkbox, ModalApp, TextField };
+export { Button, Switch, Checkbox, ModalApp, TextField, Select };
